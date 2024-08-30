3d7791603aaf86fe4202d0a886afd7adf19f4e29
VirusHeo <wrath1209@hotmail.com>
2024-08-30 17:04:53 +0900

    'Update'

--- GitCommit.bat
+++ GitCommit.bat
@@ -1,10 +1,10 @@
 @echo off
 powershell -Command "& { 
-	cd /d "D:\SILabUtility\NSIS"
-	git remote remove origin
-	git remote add origin https://github.com/seeinsidelab/SILabUtility.git 
-	git add .
-	git commit -m "Update"
-	git push -u origin main
+    cd /d 'D:\SILabUtility\NSIS'
+    git remote remove origin
+    git remote add origin https://github.com/seeinsidelab/SILabUtility.git
+    git add .
+    git commit -m 'Update'
+    git push -u origin main 
 }"
 pause
